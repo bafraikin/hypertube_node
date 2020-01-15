bfac69d9e01d5aa4cc0cde2eabf68a917aed9f2e
bafraiki <bafraiki@student.42.fr>
2020-01-15 12:54:46 +0100

    pg don't want to create a database for no reason

--- src/settings/connect_pg.ts
+++ src/settings/connect_pg.ts
@@ -3,22 +3,25 @@ import { createConnection, Connection, Repository } from "typeorm"
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 import User from '../app/models/user'
 import {yellow, red} from 'chalk'
+import { Client } from "pg";
 export let connection: Connection
 
 
 const typeOrmConfig: PostgresConnectionOptions = {
 	type: "postgres",
-	host: "localhost",
+	host: "postgres",
 	port: 5432,
 	username: "postgres",
 	password: "postgres",
-	database: "hypertube",
+	database: 'hypertube',
 	synchronize: true,
 	logging: ["query", "error", "schema", "migration"],
 	entities: [User]
-};
+}
+
 
 export async function connectWithPg() {
+	initializeDb();
 	connection =  await createConnection(typeOrmConfig);
 	return connection
 }
@@ -27,5 +30,16 @@ export function closeDBConnection () {
 	connection && connection.isConnected && connection.close()
 }
 
+const initializeDb = async function() {
+	const client = new Client({
+		password: "postgres",
+		user: "postgres",
+		host: "postgres",
+		port: 5432
+	});
+	await client.query('CREATE DATABASE hypertube', function(err) { // create user's db
+		console.log(err); // ignore if the db is there
+		client.end();
+	});
 
-
+}
