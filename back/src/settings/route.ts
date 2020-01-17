e2cb6ec3dc01bb3d8e7768fe24747adb8d0530b1
laetitia <laetitia-lejeune@live.fr>
2020-01-17 14:25:54 +0100

    sauvegarde

--- back/src/settings/route.ts
+++ back/src/settings/route.ts
@@ -4,11 +4,25 @@ import {Express } from 'express'
 import authenticate from '../app/controllers/authenticate'
 import {Request, Response} from "express";
 
+const imdb = require('imdb-node-api');
+
 export default function setRoute(connection: Connection, app: Express) {
 
 
 	app.get('/test', (req: Request, res: Response) =>{return res.send({coucou: 'salut'});})
 
+	app.get('/lala', (req: Request, res: Response) => {
+		// imdb.searchMovies('The Lion King', function (movies: string) {
+		// 	console.log(movies);
+		// }, function(error: string){
+		// 	console.error(error);
+		// });
+		imdb.getMovie('tt0110357', function (movie: string) {
+			console.log(movie);
+		}, function(error: string) { 
+			console.error(error);
+		});
+	});
 
 	app.route('/user')
 		.get	
