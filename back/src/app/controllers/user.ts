495c7a6cca090964ce840d7feb74e8a9e2f9d16f
ADejbakhsh <adejbakh@student.42.fr>
2020-01-22 16:29:37 +0100

    Forgot to add controller

--- back/src/app/controllers/user.ts
+++ back/src/app/controllers/user.ts
@@ -1,13 +1,29 @@
-
 import { User } from '../../app/models/user'
 import {Request, Response} from 'express'
-import { validationResult } from 'express-validator'
+import {validate} from 'class-validator'
 
 export default class userController {
 
 	static create(req: Request, res: Response) {
 		const user = new User();
+		user.email =req.body.email;
+		user.pseudo = req.body.pseudo;
+		user.firstname = req.body.firstname;
+		user.lastname = req.body.lastname;
+		user.password = req.body.password;
+		user.imageUrl = req.body.img;
+		validate(user).then(errors => { // errors is an array of validation errors
+				if (errors.length > 0) {
+				res.send(errors);
+				} else if(!(/([0-9].*([a-z]|[A-Z]))|(([a-z]|[A-Z]).*[0-9])/.test(req.body.password))) {
+				res.send("{error: \"Password must containe at least a number and a letter\"}");
+				} else {
+				user.save();
+				res.send("true");
+				}
+				});
 	}
+
 
 	static update_password(req: Request, res: Response) {
 
@@ -21,7 +37,5 @@ export default class userController {
 
 	}
 
-	static test(req: Request, res: Response) {
-		res.json(validationResult(req));
-	}
+
 }
