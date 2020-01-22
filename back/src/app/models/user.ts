ac8ccd621ce01e3c26745f4a64264196d2b70489
ADejbakhsh <adejbakh@student.42.fr>
2020-01-22 16:24:39 +0100

    Model and controller are ready for user creation Error are not perfect but i think it will be easier to tell mistake to user in front

--- back/src/app/models/user.ts
+++ back/src/app/models/user.ts
@@ -1,4 +1,6 @@
 import {BaseEntity, Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {IsFQDN ,IsAlphanumeric, Length, IsEmail} from "class-validator";
+
 const bcrypt = require('bcrypt');
 
 @Entity("users")
@@ -8,21 +10,30 @@ export class User extends BaseEntity {
 	id!: number;
 
 	@Column({unique: true})
+	@IsEmail()
+	@Length( 4,  254)
 	email!: string;
 
 	@Column()
-	name!: string;
+	@Length( 1,  10)
+	firstname!: string;
 
 	@Column()
-	surname!: string;
+	@Length( 1,  10)
+	lastname!: string;
 
 	@Column()
+	@Length( 1,  10)
 	pseudo!: string;
 
 	@Column()
+	@Length( 8,  254)
+	@IsAlphanumeric()
 	password!: string;
 
 	@Column()
+	@IsFQDN()
+	@Length( 3, 254)
 	imageUrl!: string;
 
 	async setPassword(pw: string) {
@@ -39,5 +50,10 @@ export class User extends BaseEntity {
 			id: this.id,
 			email: this.email
 		}
+	}
+
+	checkPass() {
+	this.password
+
 	}
 }
--- back/src/settings/route.ts
+++ back/src/settings/route.ts
@@ -3,7 +3,6 @@ import passport from 'passport';
 import {Express } from 'express'
 import * as controller from '../app/controllers/index'
 import {Request, Response} from "express";
-import { check} from 'express-validator'
 
 export default function setRoute(connection: Connection, app: Express) {
 
@@ -11,10 +10,18 @@ export default function setRoute(connection: Connection, app: Express) {
 	app.get('/test', (req: Request, res: Response) => {return res.send({coucou: 'salut'});})
 
 
-	app.route('/user').get([check('coucou').isLength({ min: 5 })], controller.user.test);
 	app.post('/login', passport.authenticate('local', controller.authenticate.authenticateObject()));
 	app.get('/logout', controller.authenticate.logout);
+	
 
+	/*
+	* La route /UserCreate permet de cree un utilisateur 
+	* Elle prend elle a besoin des info du model user.ts pour fonctionner
+	* peut etre tester a la main avec cette commande :
+	* curl -d "img=abc.fr&password=7char_and_a_int&email=email@42.fr&firstname=foo&lastname=bar&pseudo=xXx"
+	   -X POST http://localhost:3000/UserCreate
+	*/
+	app.post('/UserCreate', controller.user.create);
 
 
 
