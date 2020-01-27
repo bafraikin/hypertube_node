a1a24e68e1b76ecf24adc285a3f49ec49bdca487
bafraiki <bafraiki@student.42.fr>
2020-01-27 20:08:02 +0100

    added layout

---
+++ front/src/components/layouts/layout.vue
@@ -0,0 +1,22 @@
+
+<template>
+	<div>	
+		<navbar/>
+		<div class='container-md'>
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script>
+import Navbar from './navbar.vue'
+
+export default {
+	components: {
+		Navbar
+	}
+}
+
+
+
+</script>
---
+++ front/src/components/layouts/navbar.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div>
+    <v-app-bar color="#BF55EC" dark dense>
+
+      <v-toolbar-title>Hypertubulaire</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class Navbar  extends Vue {
+  }
+</script>
