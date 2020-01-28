28694894a86416c8f690674469b1ddbb188187f9
bafraiki <bafraiki@student.42.fr>
2020-01-28 21:19:54 +0100

    going to use an event bus 😂😂😂

---
+++ front/src/components/layouts/modal_signup.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    
+    <v-row justify="center">
+      <v-btn
+        dark
+        @click.stop="dialog = true"
+      >
+        sign up
+      </v-btn>
+
+      <v-dialog
+        v-model="dialog"
+        max-width="700"
+      >
+        <v-card>
+          <v-card-title class="headline">Creer ton compte utilisateur</v-card-title>
+
+          <v-card-text>
+            Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+              color="green darken-1"
+              text
+              @click="dialog = false"
+            >
+              Disagree
+            </v-btn>
+
+            <v-btn
+              color="green darken-1"
+              text
+              @click="dialog = false"
+            >
+              Agree
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+           dialog: false
+
+      }
+    },
+  }
+</script>
--- front/src/components/layouts/navbar.vue
+++ front/src/components/layouts/navbar.vue
@@ -1,23 +1,44 @@
-
 <template>
-  <div>
-    <v-app-bar color="#BF55EC" dark dense>
+  <v-app-bar color="#BF55EC" dark dense>
+    <v-toolbar-title>Hypertubulaire</v-toolbar-title>
 
-      <v-toolbar-title>Hypertubulaire</v-toolbar-title>
+    <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+    <sign-up/>
+    <div data-app='true'> 
+      <v-menu>
+        <template v-slot:activator="{ on }">
+          <v-btn icon  v-on="on">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
 
-      <v-btn icon>
-      <v-icon>mdi-account</v-icon>
-    </v-btn>
-    </v-app-bar>
-  </div>
+        <v-list>
+          <v-list-item
+            v-for="n in arr"
+            :key='n'
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+  </v-app-bar>
 </template>
 
-<script lang="ts">
-  import { Component, Prop, Vue } from 'vue-property-decorator';
+<script>
+  //  import buttonSignUp from './button_signup'
+  import eventBus from './bus_event.js'
 
-  @Component
-  export default class Navbar  extends Vue {
+  export default {
+    data() {
+      return {
+        arr: ['a', 'c']
+      }
+    },
+    components: {
+      //    buttonSignUp
+  } 
   }
 </script>
