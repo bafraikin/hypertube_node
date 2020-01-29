61d6f885c94d85b6a38dc663f55f37fd3fbdb35d
jchardin <jerome.chardin@live.fr>
2020-01-29 13:51:27 +0100

    add modal sign in

--- front/src/components/layouts/navbar.vue
+++ front/src/components/layouts/navbar.vue
@@ -1,44 +1,47 @@
 <template>
-  <v-app-bar color="#BF55EC" dark dense>
-    <v-toolbar-title>Hypertubulaire</v-toolbar-title>
+	<v-app-bar color="#BF55EC" dark dense>
+		<v-toolbar-title>Hypertubulaire</v-toolbar-title>
 
-    <v-spacer></v-spacer>
+		<v-spacer></v-spacer>
+		<buttonsignup></buttonsignup>
+		<buttonsignin></buttonsignin>
+		<!-- // -->
+			<div data-app='true'> 
+				<v-menu>
+					<template v-slot:activator="{ on }">
+						<v-btn icon  v-on="on">
+							<v-icon>mdi-account</v-icon>
+						</v-btn>
+					</template>
 
-    <sign-up/>
-    <div data-app='true'> 
-      <v-menu>
-        <template v-slot:activator="{ on }">
-          <v-btn icon  v-on="on">
-            <v-icon>mdi-account</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="n in arr"
-            :key='n'
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </div>
-  </v-app-bar>
+					<v-list>
+						<v-list-item
+		  v-for="n in arr"
+	:key='n'
+	@click="() => {}"
+	>
+	<v-list-item-title>Option {{ n }}</v-list-item-title>
+						</v-list-item>
+					</v-list>
+				</v-menu>
+			</div>
+	</v-app-bar>
 </template>
 
 <script>
-  //  import buttonSignUp from './button_signup'
-  import eventBus from './bus_event.js'
+import buttonSignUp from './modal_signup'
+import buttonSignIn from './modal_signin'
+//  import eventBus from './bus_event.js'
 
-  export default {
-    data() {
-      return {
-        arr: ['a', 'c']
-      }
-    },
-    components: {
-      //    buttonSignUp
-  } 
-  }
+export default {
+	data() {
+		return {
+			arr: ['a', 'c']
+		}
+	},
+	components: {
+		buttonsignup: buttonSignUp,
+		buttonsignin: buttonSignIn,
+	} 
+}
 </script>
