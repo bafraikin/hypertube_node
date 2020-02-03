02f501d0d386125f57497e5bbf2bb264d6ee1b0f
bafraiki <bafraiki@student.42.fr>
2020-02-03 13:50:07 +0100

    route in back are setup😱😱😱

--- back/src/settings/route.ts
+++ back/src/settings/route.ts
@@ -5,37 +5,33 @@ import {Express, Request, Response, Router} from "express";
 
 export default function setRoute(connection: Connection, app: Express) {
 
-
-	app.get('/test', (req: Request, res: Response) => {return 
-			
-					res.send({coucou: 'salut'});})
-
-					app.route('/authentication')
-					.post(passport.authenticate('local', controller.authenticate.authenticateObject()))
-					.delete(controller.authenticate.logout);
+	let userNotAuthenticated: Router = Router().use(controller.authenticate.checkNotAuth);
+	let userAuthenticated: Router = Router().use(controller.authenticate.checkAuth);
 
 
+	userNotAuthenticated.post('/authentication', passport.authenticate('local', controller.authenticate.authenticateObject()));
+	userNotAuthenticated.post("/user", controller.user.create);
 
+	userAuthenticated.delete("/user", controller.authenticate.logout);
 
-
-					/*
-					 * La route /UserCreate permet de cree un utilisateur 
-					 * Elle prend elle a besoin des info du model user.ts pour fonctionner
-					 * peut etre tester a la main avec cette commande :
-					 * curl -d "img=abc.fr&password=7char_and_a_int&email=email@42.fr&firstname=foo&lastname=bar&pseudo=xXx"
-					 -X POST http://localhost:3000/UserCreate
-					 */
-
-					app.post("/user", controller.user.create);
+	app.use("/", userNotAuthenticated);
+	app.use("/😱", userAuthenticated);
 
 
 
-					return app;
+	/*
+	 * La route /UserCreate permet de cree un utilisateur 
+	 * Elle prend elle a besoin des info du model user.ts pour fonctionner
+	 * peut etre tester a la main avec cette commande :
+	 * curl -d "img=abc.fr&password=7char_and_a_int&email=email@42.fr&firstname=foo&lastname=bar&pseudo=xXx"
+	 -X POST http://localhost:3000/UserCreate
+	 */
+
 
 
 
 	app.post('/film-info', controller.filmInfo.searchInfo);
-	
+
 	app.post('/film-search-api-query-string', controller.movies.ytsApiQueryString);
 	app.post('/download', controller.movies.getDownload);
 	app.get('/download/delete', controller.movies.deleteAllMovies);
