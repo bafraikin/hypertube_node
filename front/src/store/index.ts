9d16874e260c4f684664545e7ce3b6593a6b0a93
bafraiki <bafraiki@student.42.fr>
2020-02-03 17:33:36 +0100

    ongoing feature

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,15 +1,36 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { StoreOptions } from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+interface DefaultState = {
+	connected: Boolean
+}
+
+const store: StoreOptions<DefaultState> = {
+	state: {
+		connected:  false 
+	},
+	mutations: {
+		connectUser(state: StoreOptions<DefaultState>, user: Object) {
+			state.connected = true
+			state.user = user
+		},
+		disconnectUser(state) {
+			state.connected = false
+		}
+	},
+	actions: {
+	},
+	modules: {
+	},
+	getters: {
+		doneTodos: state => {
+			return state.connected
+		}
+	}
+}
+
+
+
+export default new Vuex.Store<DefaultState>(store)
