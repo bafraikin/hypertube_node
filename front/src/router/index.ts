ab34efd90492fa367764eb593c912c5b1e393c61
jchardin <jerome.chardin@live.fr>
2020-02-04 13:49:59 +0100

    je suis empty

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -19,27 +19,32 @@ const router = new VueRouter({
 	{
 		path: '/film-info',
 		name: 'film-info',
-		component: () => import('../views/Film-info.vue')
+		component: () => import('../views/Film-info.vue'),
+		meta: {layout: "layout"}
 	},
 	{
 		path: '/film-list',
 		name: 'film-list',
-		component: () => import('../views/Film-list.vue')
+		component: () => import('../views/Film-list.vue'),
+		meta: {layout: "layout"}
 	},
 	{
 		path: '/download/',
 		name: 'film-download',
-		component: () => import('../views/Film-download.vue')
+		component: () => import('../views/Film-download.vue'),
+		meta: {layout: "layout"}
 	},
 	{
 		path: '/download/',
 		name: 'film-download-seconde',
-		component: () => import('../views/Film-download.vue')
+		component: () => import('../views/Film-download.vue'),
+		meta: {layout: "layout"}
 	},
 	{
 		path: '/film-player/',
 		name: 'player-film',
-		component: () => import('../views/Film-play.vue')
+		component: () => import('../views/Film-play.vue'),
+		meta: {layout: "layout"}
 	},
 ]
 })
