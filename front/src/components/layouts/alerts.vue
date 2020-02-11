3dfc1e2c6008bdc412dd0ddbd7ae7df1ac61985f
bafraiki <bafraiki@student.42.fr>
2020-02-11 23:13:20 +0100

    going to change alert to code based and centralize message into alert components

--- front/src/components/layouts/alerts.vue
+++ front/src/components/layouts/alerts.vue
@@ -1,17 +1,31 @@
 <template>
-  <div></div>
 </template>
+
+
+
 <script>
   import bus from '@/config/bus_event'
 
-	export default {
+
+  export default {
     methods: {
+      findMessage(code) {
+        switch(code){
+          case "UNSIGNED": 
+            return "you need to sign in to perform this action";
+          default:
+            return "error"
+        }
+      },
       displayAlert(args) {
-        this.$toast[`${args.type}`](args.msg)
+        if (args.code !== undefined)
+          this.$toast[`${args.type}`](this.findMessage(args.code))
+        else
+          this.$toast[`${args.type}`](args.msg)
       }
     },
     created() {
       bus.$on('alert', this.displayAlert);
     }
-	}
+  }
 </script>
