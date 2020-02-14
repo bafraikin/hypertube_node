08a4904d7f7eb11ad3562153a3c4d97a4beafb41
jchardin <jerome.chardin@live.fr>
2020-02-14 18:19:07 +0100

    front research ongoing

--- front/src/components/ResearchBar.vue
+++ front/src/components/ResearchBar.vue
@@ -1,12 +1,60 @@
 <template>
-	<v-container dark>
-		<h1 class="ici">Hello you</h1>
-	</v-container>
+<v-container>
+<v-card flat color="transparent">
+
+    <v-subheader>Range year movie</v-subheader>
+
+    <v-card-text>
+      <v-row>
+        <v-col class="px-4">
+          <v-range-slider
+            v-model="range"
+            :max="max"
+            :min="min"
+            hide-details
+            class="align-center"
+          >
+            <template v-slot:prepend>
+              <v-text-field
+                :value="range[0]"
+                class="mt-0 pt-0"
+                hide-details
+                single-line
+                type="number"
+                style="width: 60px"
+                @change="$set(range, 0, $event)"
+              ></v-text-field>
+            </template>
+            <template v-slot:append>
+              <v-text-field
+                :value="range[1]"
+                class="mt-0 pt-0"
+                hide-details
+                single-line
+                type="number"
+                style="width: 60px"
+                @change="$set(range, 1, $event)"
+              ></v-text-field>
+            </template>
+          </v-range-slider>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+
+
+
+
+
+</v-container>
 </template>
 
 <style>
 .ici{
-	color:white;
+color:white;
+}
+.other{
+	background-color:white;
 }
 </style>
 
@@ -14,6 +62,13 @@
 import Comment from '@/components/Comments.vue'
 
 export default {
+data () {
+      return {
+        min: 1980,
+        max: 2020,
+        range: [2005, 2018],
+      }
+	} ,
 	props: {
 	},
 	components: {
