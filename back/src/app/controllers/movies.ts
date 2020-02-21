b82183842e7ee3700a72dcb2475715711b68d955
jchardin <jerome.chardin@live.fr>
2020-02-21 13:21:00 +0100

    error handling

--- back/src/app/controllers/movies.ts
+++ back/src/app/controllers/movies.ts
@@ -7,12 +7,12 @@ import axios from 'axios';
 export default class moviesController {
 
 	static async searchForMovies(req: Request, res: Response) {
+		try {
 		let url: string;
 
 		let apiClient : any;
 		if (!req.query.page)
-			return(res.status(400).send("error"));
-
+			throw "error";
 		if (req.query.queryString == undefined || req.query.queryString == '') {
 			let filter = req.query;
 			let movieFilter: Filter = {firstYear: Number(filter.firstYear), lastYear: Number(filter.lastYear), minMark: Number(filter.minMark), maxMark: Number(filter.maxMark), genders: filter.gender};
@@ -21,23 +21,33 @@ export default class moviesController {
 		else
 			apiClient = new TMDBClientSearch(req.query.queryString);
 			apiClient.getPage(req.query.page).then((response: any)  => {res.send(response.data.results)}).catch((err:any)  => { res.status(401).send("error") });
-		return;
+		} catch (err) {
+			res.status(400).send("error")
+		}
 	}
 
 	static async getMovieDetail(req: Request, res: Response) {
-		let OMDBid = req.query.OMDBid;
-		let url = "https://api.themoviedb.org/3/movie/" + OMDBid + "?api_key=" + process.env.OMDB_KEY + "&language=en-US";
-		let response = await axios.get(url);
-		let movieDetail = response.data;
-		res.send(movieDetail);
+		try {
+			let OMDBid = req.query.OMDBid;
+			let url = "https://api.themoviedb.org/3/movie/" + OMDBid + "?api_key=" + process.env.OMDB_KEY + "&language=en-US";
+			let response = await axios.get(url);
+			let movieDetail = response.data;
+			res.send(movieDetail);
+		} catch (err) {
+			res.status(400).send("error")
+		}
 	}
 
 	static async getMovieCasting(req: Request, res: Response) {
-		let OMDBid = req.query.OMDBid;
-		let url = "https://api.themoviedb.org/3/movie/" + OMDBid + "/credits" + "?api_key=" + process.env.OMDB_KEY;
-		let response = await axios.get(url);
-		let movieDetail = response.data;
-		res.send(movieDetail);
+		try {
+			let OMDBid = req.query.OMDBid;
+			let url = "https://api.themoviedb.org/3/movie/" + OMDBid + "/credits" + "?api_key=" + process.env.OMDB_KEY;
+			let response = await axios.get(url);
+			let movieDetail = response.data;
+			res.send(movieDetail);
+		} catch (err) {
+			res.status(400).send("error")
+		}
 	}
 
 }
