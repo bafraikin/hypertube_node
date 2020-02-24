1a8f58ecd50e99cabc3db6d03830875de5fa5b78
bafraiki <bafraiki@student.42.fr>
2020-02-24 20:26:50 +0100

    improved a bit commentary

--- front/src/components/Comments.vue
+++ front/src/components/Comments.vue
@@ -1,58 +1,73 @@
 <template>
-	<div style="color: white">
-		<h2>Comments</h2>
-		<div v-for="comment in comments">
-			<p v-on:click="displayUserProfile(comment.user.id)">{{ comment.user.login }}</p>
-			<p>{{ comment.content }}</p>
-		</div>
-		<v-form>
-			<v-textarea
-			v-model="textarea"
-			label="Please enter your comment"
-   dark
-			></v-textarea>
-			<v-btn class="mr-4" @click="submit">submit</v-btn>
-		</v-form>
-	</div>
+  <v-card black id="comment">
+    <v-list two-line>
+      <template v-for="(comment, index) in comments">
+        <v-subheader v-if="index == 0"> Comments </v-subheader>
+        <v-divider v-if="index != 0" :inset="true"></v-divider>
+        <v-list-item :key="comment.content" @click="displayUserProfile(comment.user.id)" shaped>
+          <v-list-item-avatar>
+            <img src='https://resize-elle.ladmedia.fr/rcrop/638,,forcex/img/var/plain_site/storage/images/people/la-vie-des-people/news/pamela-anderson-son-evolution-de-malibu-a-aujourd-hui/81439406-1-fre-FR/Pamela-Anderson-son-evolution-de-Malibu-a-aujourd-hui.jpg'/>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title> {{comment.user.login}} </v-list-item-title>
+            <v-list-item-subtitle> {{comment.content }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+    <v-form>
+      <v-textarea
+        v-model="textarea"
+        label="Please enter your comment"
+        dark
+      ></v-textarea>
+      <v-btn class="mr-4" @click="submit">submit</v-btn>
+    </v-form>
+  </v-card>
 </template>
 
-
-
 <script>
-import axios from  '@/config/axios_default';
+  import axios from  '@/config/axios_default';
 
-export default {
-	props: {
-		imdbCode: { type: String },
-	},
-	data() {
-		return {
-			comments: null,
-			content: null,
-			textarea: null,
-		}
-	},
-	methods:{
-		displayUserProfile(userId){
-				this.$router.push({ name: "userProfile", params:{userId: userId}});
-		},
-		getComments(){
-			axios.get('😂/comment', { params: { imdbCode: this.imdbCode }
-				}) .then(response => { this.comments = response.data; })
-		},
-		submit(event){
-			event.preventDefault();
-			this.postComment();
-		},
-		postComment(){
-			axios.post('😂/comment', { imdbCode: this.imdbCode, content: this.textarea,
-				}) .then(response => { this.getComments(); })
-			this.textarea = null;
-		},
-	},
-	mounted(){
-		this.getComments()
-	}
-}
+  export default {
+    props: {
+      imdbCode: { type: String },
+    },
+    data() {
+      return {
+        comments: null,
+        content: null,
+        textarea: null,
+      }
+    },
+    methods:{
+      displayUserProfile(userId){
+        this.$router.push({ name: "userProfile", params:{userId: userId}});
+      },
+      getComments(){
+        axios.get('😂/comment', { params: { imdbCode: this.imdbCode }
+        }) .then(response => { this.comments = response.data; })
+      },
+      submit(event){
+        event.preventDefault();
+        this.postComment();
+      },
+      postComment(){
+        axios.post('😂/comment', { imdbCode: this.imdbCode, content: this.textarea,
+        }) .then(response => { this.getComments(); })
+        this.textarea = null;
+      },
+    },
+    mounted(){
+      this.getComments()
+    }
+  }
 
 </script>
+
+<style type="scss" media="screen">
+  #comment {
+    text-align: initial;
+  }
+  
+</style>
