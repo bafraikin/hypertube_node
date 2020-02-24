33925f14c1f8c4bfaa5e893d3b588b6d49ba8952
jchardin <jerome.chardin@live.fr>
2020-02-24 20:07:37 +0100

    le retour my bad remove usless route

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -10,29 +10,41 @@ const router = new VueRouter({
 	mode: 'history',
 	routes: [
 		{
+			path: '/',
+			name: 'Home',
+			component: () => import('../../src/components/NewHome.vue'),
+				meta: {layout: "layout"}
+		},
+		{
 			path: '/player/:idOMDB/:magnetLink',
 			name: 'player-film',
 			component: () => import('../views/Film-play.vue'),
-			meta: {layout: "layout"}
+				meta: {layout: "layout"}
 		},
 		{
 			path: '/user/:userId',
 			name: 'userProfile',
 			component: () => import('../views/Profile.vue'),
-			meta: {layout: "layout"}
-		},
-		{
-			path: '/',
-			name: 'Home',
-			component: () => import('../../src/components/NewHome.vue'),
-			meta: {layout: "layout"}
+				meta: {layout: "layout"}
 		},
 		{
 			path: '/movie/:OMDBid',
 			name: 'movie',
 			component: () => import('../../src/components/MovieDetails.vue'),
-			meta: {layout: "layout"}
+				meta: {layout: "layout"}
 		},
+		{
+			path: '/oauth42/callback',
+			name: 'oauth42',
+			component: () => import('../views/oauth42.vue'),
+				meta: {layout: "layout"}
+		},
+		{
+			path: '/oauthGoogle/callback',
+			name: 'oauthGoogle',
+			component: () => import('../views/oauthGoogle.vue'),
+				meta: {layout: "layout"}
+		}
 	]
 })
 
