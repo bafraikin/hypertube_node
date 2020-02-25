d1f4bd9cfdc9569914a0228689b063120fbb6624
laetitia <laetitia-lejeune@live.fr>
2020-02-25 21:27:46 +0100

    Revert "cancel lang url"
    This reverts commit 89da51678cee2e068cb7c5f1f7c8bd12d16a42aa.

--- front/src/main.ts
+++ front/src/main.ts
@@ -19,6 +19,19 @@ Vue.use(VuetifyToast, {
 });
 Vue.use(VueI18n);
 
+// const messages = {
+//   en: {
+//     message: {
+//       hello: 'hello world'
+//     }
+//   },
+//   ja: {
+//     message: {
+//       hello: 'こんにちは、世界'
+//     }
+//   }
+// }
+
 export const i18n = new VueI18n({
   locale: 'en',
   fallbackLocale: 'en',
