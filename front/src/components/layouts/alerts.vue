5c6ff315ae3e7744a488c2384fb1f75d8e3e26c2
laetitia <laetitia-lejeune@live.fr>
2020-02-26 18:29:55 +0100

    solution temp traduction message alert + reste a tester erreur oauth

--- front/src/components/layouts/alerts.vue
+++ front/src/components/layouts/alerts.vue
@@ -8,18 +8,60 @@
   export default {
     methods: {
       findMessage(code) {
+        let language = this.$i18n.locale;
+        console.log(this.$i18n);
         switch(code){
           case "UNSIGNED":
-            return "you need to sign in to perform this action";
+            return this.whichMess(language, "needsignin");
           case "NOT_ALLOWED":
-            return "you're not allowed to perform this";
+            return this.whichMess(language, "notallowed");
           case "BAD_INPUT":
-            return "bad input"
+            return this.whichMess(language, "badinput");
           case "CREATED":
-            return "action granteed";
+            return this.whichMess(language, "granteed");
           default:
             return "error"
         }
+      },
+      whichMess(lang, mess){
+        switch(mess){
+          case "granteed":
+            if (lang == "fr"){
+              return this.$i18n.messages.fr.granteed;
+            }
+            else{
+              return this.$i18n.messages.en.granteed;
+            }
+          case "badinput":
+            if (lang == "fr"){
+              return this.$i18n.messages.fr.badinput;
+            }
+            else{
+              return this.$i18n.messages.en.badinput;
+            }
+          case "notallowed":
+            if (lang == "fr"){
+              return this.$i18n.messages.fr.notallowed;
+            }
+            else{
+              return this.$i18n.messages.en.notallowed;
+            }
+          case "needsignin":
+            if (lang == "fr"){
+              return this.$i18n.messages.fr.needsignin;
+            }
+            else{
+              return this.$i18n.messages.en.needsignin;
+            }
+          default:
+            if (lang == "fr"){
+              return "erreur";
+            }
+            else{
+              return "error";
+            }             
+        }
+
       },
       displayAlert(args) {
         if (args.code !== undefined)
