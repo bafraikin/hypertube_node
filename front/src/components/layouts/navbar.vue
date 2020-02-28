59f0d60620aa600e1b9df0fc91a254435f91f04c
bafraiki <bafraiki@student.42.fr>
2020-02-28 16:55:25 +0100

    improvement but still need some upgrade

--- front/src/components/layouts/navbar.vue
+++ front/src/components/layouts/navbar.vue
@@ -1,59 +1,90 @@
 <template>
-   <v-card
+  <v-card
     class="mx-auto overflow-hidden"
     tile
   >
-  <v-app-bar
-    color="deep-purple accent-4"
-    dense
-    dark
-  >
-    <v-app-bar-nav-icon class="d-sm-none" @click.stop="showMenu = !showMenu"></v-app-bar-nav-icon>
-    <v-toolbar-title>
-      <h3 >
-          <router-link to="/" v-slot="{ href, route, navigate}">
-          <a :href="href" @click="navigate">Hypertubulaire</a>
-        </router-link>
-    </h3>
-  </v-toolbar-title>
-    <v-spacer class="d-none d-sm-flex"></v-spacer>
-
-    <v-btn-toggle class="d-none d-sm-flex" v-if="!isConnected"
-      shaped
-      borderless
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
     >
-      <sign-up/>
-      <sign-in/>
-    </v-btn-toggle>
+      <v-app-bar-nav-icon class="d-sm-none" @click.stop="showMenu = !showMenu"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <h3 >
+          <router-link to="/" v-slot="{ href, route, navigate}">
+            <a :href="href" @click="navigate">Hypertubulaire</a>
+          </router-link>
+        </h3>
+      </v-toolbar-title>
+      <v-spacer class="d-none d-sm-flex"></v-spacer>
 
-    <v-menu  v-else light bottom left nudge-bottom="35"    v-model="showMenuUser">
-      <template v-slot:activator="{ on }">
-        <v-btn icon  class="d-none d-sm-flex" v-on="on">
-          <v-icon>mdi-account</v-icon>
-        </v-btn>
-      </template>
+      <v-btn-toggle class="d-none d-sm-flex" v-if="!isConnected"
+        shaped
+        borderless
+      >
+        <sign-up/>
+        <sign-in/>
+      </v-btn-toggle>
 
-      <v-list data-app="true">
-        <v-list-item
-          v-for="n in arr"
-          :key="n.message"
-          @click="() => {}"
+      <v-menu  v-else light bottom left nudge-bottom="35"    v-model="showMenuUser">
+        <template v-slot:activator="{ on }">
+          <v-btn icon  class="d-none d-sm-flex" v-on="on">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list data-app="true">
+          <v-list-item
+            v-for="n in arr"
+            :key="n.message"
+            @click="() => {}"
+          >
+            <v-list-item-title @click="n.method">{{ n.message }}</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <router-link :to="{ name: 'userProfile', params: { userId: $store.state.user.id }}" v-slot="{ href, route, navigate}">
+              <v-list-item-title>
+                <a :href="href" style="text-decoration: none; color: unset" @click="navigate">profil</a>
+              </v-list-item-title>
+            </router-link>
+          </v-list-item>
+          <v-list-item>
+            <modify-profil />
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      class="deep-purple accent-4"
+      dark
+      fixed
+      temporary
+      v-model="showMenu"
+    >
+      <v-app-bar-nav-icon class="d-sm-none" @click.stop="showMenu = !showMenu"></v-app-bar-nav-icon>
+      <v-list>
+        <v-list-item v-if="false"
+          v-for="item in items"
+          :key="item.title"
+          link
         >
-          <v-list-item-title @click="n.method">{{ n.message }}</v-list-item-title>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
-         <v-list-item>
-                <router-link :to="{ name: 'userProfile', params: { userId: $store.state.user.id }}" v-slot="{ href, route, navigate}">
-                  <v-list-item-title>
-                    <a :href="href" style="text-decoration: none; color: unset" @click="navigate">profil</a>
-                  </v-list-item-title>
-                </router-link>
-              </v-list-item>
-              <v-list-item>
-                <modify-profil />
-              </v-list-item>
       </v-list>
-    </v-menu>
-  </v-app-bar>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block @click.stop="logout">Logout</v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
   </v-card>
 </template>
 
@@ -95,10 +126,10 @@
 </script>
 
 <style lang="scss" media="screen">
-    h3 a {
-      text-decoration: none;
-      color: #00ffdf!important;
-    }
+  h3 a {
+    text-decoration: none;
+    color: #00ffdf!important;
+  }
   .v-menu__content {
     left: 0;
     max-width: 100%;
